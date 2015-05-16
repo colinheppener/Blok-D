6bfb6f93eecf96d4acedf230fe1474992583292f
colinheppener <colin_heppener@live.nl>
2015-05-16 12:43:04 +0200

    opzet grid van project, en wat gekloot met dijkstra

--- ProjectDJava/src/projectdjava/Speelbord.java
+++ ProjectDJava/src/projectdjava/Speelbord.java
@@ -9,16 +9,31 @@ package projectdjava;
  * @author Colin
  */
 public class Speelbord {
-    int huidigLevel;
-    int[][] grid = new int[20][20];
+    int huidigLevel = 1;
+    Item[][] grid = new Item[20][20];
     
     
     private void createVelden()
     {
+        Muur muur = new Muur();
+        Speler speler = new Speler();
+        Valsspeler vspeler = new Valsspeler();
+        Vriend vriend = new Vriend();
+        Bazooka bazooka = new Bazooka();
+        Helper helper = new Helper();
         
+        
+        if(huidigLevel == 1)
+        {
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    grid[i][j] = speler;
+                }
+            }
+        }
     }
     
-    public int[][] getGrid()
+    public Item[][] getGrid()
     {
         return grid;
     }
