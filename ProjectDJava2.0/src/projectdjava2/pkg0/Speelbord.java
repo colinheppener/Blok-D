8d1945d5081700765cc77ce731ead8e508de4123
colinheppener <colin_heppener@live.nl>
2015-05-27 13:56:19 +0200

    getburen gefixt

--- ProjectDJava2.0/src/projectdjava2/pkg0/Speelbord.java
+++ ProjectDJava2.0/src/projectdjava2/pkg0/Speelbord.java
@@ -40,49 +40,93 @@ public class Speelbord extends JComponent {//deze JComponent wordt weergegeven o
         {1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1,},
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 4, 1,}
     };
-
+    
     public Veld[][] getGrid() {
         return grid;
     }
-
+    
     public Speelbord() {
         CreateVelden();
     }
-
+    
     private void CreateVelden() {
-
+        
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 if (opzetGrid[i][j] == 1) {
                     Point locatie = new Point(i, j);
                     Muur muur = new Muur(locatie);
-                    grid[i][j] = new Veld(locatie, 1);
+                    grid[i][j] = new Veld(locatie, muur);
                 }
                 if (opzetGrid[i][j] == 3) {
                     Point locatie = new Point(i, j);
                     Speler speler = new Speler(locatie);
-                    grid[i][j] = new Veld(locatie, 3);
+                    grid[i][j] = new Veld(locatie, speler);
                 }
                 if (opzetGrid[i][j] == 4) {
                     Point locatie = new Point(i, j);
                     Vriend vriend = new Vriend(locatie);
-                    grid[i][j] = new Veld(locatie, 4);
+                    grid[i][j] = new Veld(locatie, vriend);
                 }
                 if (opzetGrid[i][j] == 0) {
                     Point locatie = new Point(i, j);
                     DefaultItem defaultItem = new DefaultItem(locatie);
-                    grid[i][j] = new Veld(locatie, 0);
+                    grid[i][j] = new Veld(locatie, defaultItem);
                 }
             }
         }
+        getBuren();
     }
-
+    
+    public void getBuren() {
+        HashMap buren = new HashMap<String, Veld>();
+        Veld buur;
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if(j<19){
+                buur = grid[i][j+1];
+                buren.put("oost", buur);
+                }
+                else
+                {
+                    buren.put("oost", null);
+                }
+                if (i != 0) {
+                    buur = grid[i - 1][j];
+                    buren.put("noord", buur);
+                }
+                else
+                {
+                    buren.put("noord", null);
+                }
+                if(i<19){
+                buur = grid[i + 1][j];
+                buren.put("zuid", buur);
+                }
+                else
+                {
+                    buren.put("zuid", null);
+                }
+                if (j != 0) {
+                    buur = grid[i][j - 1];
+                    buren.put("west", buur);
+                }
+                else
+                {
+                    buren.put("west", null);
+                }
+                grid[i][j].setBuren(buren);
+                System.out.println("I: "+i +"J: "+j);
+            }
+        }
+    }
+    
     private void HerstartLevel() {
     }
-
+    
     private void StartNieuwLevel() {
     }
-
+    
     @Override
     public void paintComponent(Graphics g) {//hier worden de vierkanten daadwerkelijk samengevoegd 
         //en op het speelbord weergegeven, de velden tekenen zichzelf wel met de Teken methode
@@ -91,18 +135,12 @@ public class Speelbord extends JComponent {//deze JComponent wordt weergegeven o
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 Item veldItem = grid[i][j].getItem();
-
+                
                 veldItem.Teken(g);
-
+                
             }
-
+            
         }
-
-    }
-
-    public void replaceItemOpLocatie(Point locatie) {
-        grid[(int) locatie.getX()][(int) locatie.getY()] = new Veld(locatie, 3);
-        repaint();
-        System.out.println("replace initiated");
+        
     }
 }
--- ProjectDJava2.0/src/projectdjava2/pkg0/Veld.java
+++ ProjectDJava2.0/src/projectdjava2/pkg0/Veld.java
@@ -14,16 +14,14 @@ import java.util.HashMap;
 public class Veld {
 
     private static Point locatie;
-    private int itemOpVeldID;//dit is het idee wat meegegeven wordt vanuit de CreateVelden methode in Speelbord
     private Item ItemOpVeld;
     private HashMap buren;
 
-    public Veld(Point loc, int itemID)//constructor waarin de waarden van de klassevariabelen bepaald worden, 
+    public Veld(Point loc, Item item)//constructor waarin de waarden van de klassevariabelen bepaald worden, 
     //vanuit deze constructor wordt ook de methode om de items aan te maken aangeroepen
     {
         locatie = loc;
-        itemOpVeldID = itemID;
-        createItem();
+        ItemOpVeld = item;
         
     }
 
@@ -32,63 +30,30 @@ public class Veld {
         return (Veld)buren.get(richting);
     }
     
-    public HashMap getBuren(Point locatie) {
-        Speelbord speelbord = new Speelbord();
-        Veld[][] grid = speelbord.getGrid();
-        buren = new HashMap();
-        Item buur = grid[(int) locatie.getY()][(int) locatie.getX() + 1].getItem();
-        buren.put(1, buur);
-        if((int)locatie.getY() != 0)
-        {
-        buur = grid[(int) locatie.getY() - 1][(int) locatie.getX()].getItem();
-        buren.put(0, buur);
-        }
-        buur = grid[(int) locatie.getY() + 1][(int) locatie.getX()].getItem();
-        buren.put(2, buur);
-        if((int)locatie.getX() != 0)
-        {
-        buur = grid[(int) locatie.getY()][(int) locatie.getX() - 1].getItem();
-        buren.put(3, buur);
-        }
-        return buren;
-    }
+
 
     public Point getLocatie()//hiermee kan de locatie van het veld worden opgehaald en gebruikt worden in andere methodes.
     {
         return locatie;
     }
 
-    private void createItem()//hier wordt het daadwerkelijke item op het veld gecreeerd. Het wordt dus door veld zelf gedaan en niet meer door speelbord.
-    {
-//        if(itemOpVeldID==0)//als itemID 0 is wordt er nu geen item op het veld gezet, DIT IS WAT DE NULLPOINTER VEROORZAAKT
-//        {
-// 
-//        }
-        if (itemOpVeldID == 1)//als itemID 1 is wordt muur op veld gecreeerd.
-        {
-            Muur muur = new Muur(getLocatie());
-            ItemOpVeld = muur;
-        }
-        if (itemOpVeldID == 3)//als itemID 3 is wort speler gecreeerd
-        {
-            Speler speler = new Speler(getLocatie());
-            ItemOpVeld = speler;
-        }
-        if (itemOpVeldID == 4)//als itemID 4 is wordt vriend gecreeeerd
-        {
-            Vriend vriend = new Vriend(getLocatie());
-            ItemOpVeld = vriend;
-        }
-        if (itemOpVeldID == 0) {
-            DefaultItem defaultItem = new DefaultItem(getLocatie());
-            ItemOpVeld = defaultItem;
-        }
-//        System.out.println("itemopveld:" + ItemOpVeld.toString());//hier wordt weergegeven(met de toString methodes uit de Speler en Muur klasse(tot nu toe) 
-        //welke items aan de grid toegevoegd worden(wordt weergegeven in de console.
-    }
 
     public Item getItem()//hiermee wordt het item wat op het veld staat opgehaald, dit wordt gebruikt om het juiste item te tekenen e.d.
     {
         return ItemOpVeld;
+    }
+    
+    public void setItem(Item item)
+    {
+        ItemOpVeld = item;
+    }
+    
+    public void setBuren(HashMap bur)
+    {
+        buren = bur;
+        System.out.println(buren.get("noord") +"     noord!");
+        System.out.println(buren.get("zuid") +"      zuid!");
+        System.out.println(buren.get("west") +"      west!");
+        System.out.println(buren.get("oost") +"      oost!");
     }
 }
