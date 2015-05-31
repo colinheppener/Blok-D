161f6a6449b68f8e47096785cb5c43c0bad29ef7
colinheppener <colin_heppener@live.nl>
2015-05-31 18:31:16 +0200

    opvolging van vorige commit, was niet doorgekomen

---
+++ ProjectDJava2.0/src/projectdjava2/pkg0/Bazooka.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectdjava2.pkg0;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Colin
+ */
+public class Bazooka extends Item{
+        @Override
+    public void Teken(Graphics g) {
+        g.setColor(Color.MAGENTA);
+
+//        g.fillRect((int) veld.getY() * 40, (int) veld.getX() * 40, 40, 40);
+        g.drawImage(getImageFile(), (int) veld.getY() * 40, (int) veld.getX() * 40, 40, 40, null);
+
+    }
+
+    @Override
+    public String toString() {
+        return "vriend";
+    }
+
+    public BufferedImage getImageFile() {
+        try {
+            plaatje = ImageIO.read(new File("src/images/bazooka.png"));
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        return plaatje;
+    }
+}
