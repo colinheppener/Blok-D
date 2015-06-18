afe7ee1c28c5348d93007c0288fa5b2687f128d0
colinheppener <colin_heppener@live.nl>
2015-06-18 20:40:32 +0200

    booleans eruit

--- ProjectDJava2.0/src/projectdjava2/pkg0/Frame.java
+++ ProjectDJava2.0/src/projectdjava2/pkg0/Frame.java
@@ -22,8 +22,6 @@ public final class Frame extends JFrame {
     private JProgressBar timerBar;
     private JFrame frame = new JFrame();
     private Speelbord speelbord;
-    private boolean optPaneCreated = false;
-    private boolean optPaneYesOption = false;
 
     public Frame() {
         createComponents();
@@ -120,7 +118,6 @@ public final class Frame extends JFrame {
 
     private int setInterval() {
         if (interval < 1) {
-            optPaneCreated = true;
             timer.cancel();
             Object[] choices = {"Opnieuw proberen", "Naar menu"};
             Object defaultChoice = choices[0];
@@ -131,9 +128,7 @@ public final class Frame extends JFrame {
                     JOptionPane.QUESTION_MESSAGE,
                     null,
                     choices,
-                    defaultChoice);
-     
-                    
+                    defaultChoice);                    
             if (dialogResult == JOptionPane.YES_OPTION) {
                 frame.dispose();
                 frame = new Frame();
